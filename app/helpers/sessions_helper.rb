515759ec002547528cf071a6c5327f236222f1ba
Justin <beanjustinr@gmail.com>
2015-12-01 11:38:58 -0500

    users edit, update, index, and destroy

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -30,6 +30,10 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
   def forget(user)
     user.forget
     cookies.delete(:user_id)
@@ -40,5 +44,14 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
   end
 end
